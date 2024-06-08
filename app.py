f30cf9a4e15596394925d86f3a1480ab7858d643
Syed Adnan Ahmed <aadjj41@gmail.com>
2024-06-08 11:38:55 +0530

    8 commit

--- app.py
+++ app.py
@@ -5,57 +5,46 @@ from sklearn.svm import SVC
 import joblib
 import easyocr
 
-# Load the trained SVM model and TF-IDF vectorizer
 svm_model = joblib.load('svm_model.pkl')
 tfidf_vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 
-# Function to classify text as offensive or non-offensive
 def classify_text(text):
     text_tfidf = tfidf_vectorizer.transform([text])
     return svm_model.predict(text_tfidf)[0]
 
 
-# Function to extract text from an image using EasyOCR
 def extract_text(image):
-    reader = easyocr.Reader(['en'])  # Specify language (e.g., English)
+    reader = easyocr.Reader(['en'])
     result = reader.readtext(image)
     text = ' '.join([entry[1] for entry in result])
     return text
 
 
-# Function to process an image and display the result
 def process_image(image):
-    # Convert BytesIO object to bytes array
     image_bytes = image.read()
 
-    # Extract text from the image using EasyOCR
     text = extract_text(image_bytes)
 
-    # Classify the extracted text
     classification = classify_text(text)
 
-    # Display the result
     if classification == 1:
         st.error("Offensive Content Detected!")
     else:
         st.success("No Offensive Content Detected!")
 
 
-# Streamlit UI
-st.set_page_config(page_title="Cyberbullying Detection", page_icon=":shield:", layout="wide")
-st.title('Cyberbullying Detection')
-st.write("A free to use website for Cyberbullying Detection.")
+st.set_page_config(page_title="Hate Speech Detection", page_icon=":shield:", layout="wide")
+st.title('Hate Speech Detection')
+st.write("A free to use website for Hate Speech and Offensiveness Detection.")
 st.markdown("---")
 
-# Sidebar
-st.sidebar.title("Cyberbullying Detection")
+st.sidebar.title("Hate Speech Detection")
 option = st.sidebar.selectbox(
     'Select Input Type',
     ('Text', 'Image')
 )
 
-# Main content
 if option == 'Text':
     st.subheader("Enter Text:")
     input_text = st.text_area("", "", height=150)
@@ -73,7 +62,6 @@ elif option == 'Image':
         with st.spinner('Processing image...'):
             process_image(uploaded_file)
 
-# Footer
 st.markdown("---")
 st.write("Developed by Syed Adnan Ahmed,"
          " Aitham Meghana,"
